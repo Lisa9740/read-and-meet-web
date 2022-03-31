c7e2bb7bf28ed288ff695e296772b2b6028d8345
Alison Barret <alison@seyes.re>
2022-03-31 16:12:19 +0400

    Changed title register screen font

--- lib/view/screens/auth/register/register.screen.dart
+++ lib/view/screens/auth/register/register.screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:read_and_meet_web/view/screens/auth/register/components/register.form.dart';
 import 'package:read_and_meet_web/view/widgets/auth/auth.container.dart';
 
@@ -28,8 +29,8 @@ class _RegisterPageState extends State<RegisterScreen> {
                 children: <Widget>[ AuthContainer(child: Padding(padding: EdgeInsets.only(top:100, bottom: 100),child:Column(
                     children: <Widget>[
                       SizedBox(height: 50, width: 50, child: const Logo()),
-                      const Center(child: Padding(padding: EdgeInsets.only(top: 100, bottom : 30), child: Text("Inscrivez-vous ! ",
-                          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 20)))),
+                       Center(child: Padding(padding: EdgeInsets.only(top: 100, bottom : 30), child: Text("Inscrivez-vous ! ",
+                        style: GoogleFonts.raleway(fontWeight: FontWeight.bold, fontSize: 25)))),
                       RegisterForm()
                     ]
                 ))
